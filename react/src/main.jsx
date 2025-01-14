e6b74ff0c542a9c03658e2031b34b939ebb108bd
Dildora <48212@edu.raseko.fi>
2025-01-14 19:29:39 +0200

    part6-2 tutorial added

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -7,19 +7,12 @@ import noteReducer from './reducers/noteReducer';
 import filterReducer from './reducers/filterReducer.js';
 import App from './App.jsx';
 
-import { createNote } from './reducers/noteReducer'
-import { filterChange } from './reducers/filterReducer'
-
 const reducer = combineReducers({
   notes: noteReducer,
   filter: filterReducer
 });
 
 const store = createStore(reducer);
-
-store.subscribe(() => console.log(store.getState()))
-store.dispatch(filterChange('IMPORTANT'))
-store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
